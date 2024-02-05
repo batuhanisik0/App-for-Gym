4ca39677a952882e9c45bf056588e994b05c7587
batuhanisik0 <144622973+batuhanisik0@users.noreply.github.com>
2024-02-04 21:39:44 -0500

    Add files via upload

---
+++ Swift Workshop/ContentView.swift
@@ -0,0 +1,78 @@
+//
+//  ContentView.swift
+//  Swift Workshop
+//
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Image ("BackgroundImage")
+                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0), resizingMode: .tile)
+                .frame(height: 900.0)
+            
+            VStack {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .padding(.bottom, 0.0)
+                
+                HStack{
+                    Button(action: {signIn()}, label:  {Image(systemName: "person.crop.circle.badge.checkmark.fill")
+                            .renderingMode(.original)
+                            .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                            .aspectRatio(contentMode: .fit)
+                        .padding(50.0)})
+                    .buttonStyle(.plain)
+                    
+                    Button(action: {
+                        signOut()
+                    }, label: {Image(systemName: "person.crop.circle.badge.xmark.fill")
+                            .renderingMode(.original)
+                            .resizable(capInsets: EdgeInsets())
+                            .aspectRatio(contentMode: .fit)
+                        .padding(50.0)})
+                    .buttonStyle(.plain)
+                }
+                .padding(.vertical, 200)
+                
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    func signIn() {
+        print("check in")
+//        TabView {
+//            .tabItem {
+//                Text("Hello")
+//                Image(systemName: "message.fill")}}
+//        Text("Message")
+    }
+    
+    
+    
+    func signOut() {
+        print("check out")
+    }
+    
+}
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swift Workshop/Swfit_WorkshopApp.swift
@@ -0,0 +1,16 @@
+//
+//  Swfit_WorkshopApp.swift
+//  Swfit Workshop
+//
+//
+
+import SwiftUI
+
+@main
+struct Swfit_WorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
